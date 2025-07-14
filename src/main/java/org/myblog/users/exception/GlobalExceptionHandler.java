29575c2cccf7ea9247d8726f0b0a4e271c7c70fe
Maxim Smagin <maxprogger@mail.ru>
2025-07-14 19:48:53 +0700

    Add logging for 'GlobalExceptionHandler'

--- src/main/java/org/myblog/users/exception/GlobalExceptionHandler.java
+++ src/main/java/org/myblog/users/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package org.myblog.users.exception;
 
+import org.apache.logging.log4j.Logger;
 import org.myblog.users.dto.AppResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +13,9 @@ import java.util.TreeSet;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @Autowired
+    private Logger logger;
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handle(MethodArgumentNotValidException ex) {
         AppResponse<?> result = new AppResponse<>(new TreeMap<>());
@@ -22,16 +27,22 @@ public class GlobalExceptionHandler {
                     result.getErrors().get(i.getField()).add(i.getDefaultMessage());
                 });
 
+        logger.error(String.format("Bad request: %s", result.toString()));
+
         return ResponseEntity.badRequest().body(result);
     }
 
     @ExceptionHandler(RestIllegalArgumentException.class)
     public ResponseEntity<?> handle(RestIllegalArgumentException ex) {
+        logger.error(String.format("Bad request: %s - %s", ex.getField(), ex.getMessage()));
+
         return ResponseEntity.badRequest().body(new AppResponse<>().addErrorFluent(ex.getField(), ex.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handle(Exception ex) {
+        logger.error(String.format("Bad request: %s", ex.getMessage()));
+
         return ResponseEntity.badRequest().body(new AppResponse<>().addErrorFluent(ex.getMessage()));
     }
 }
